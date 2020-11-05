6369af38d565ca36cd79a23a7276ca9a462ab548
MCKRosario <mck.rosario@gmail.com>
2020-11-04 20:15:13 -0500

    Se agrego una alerta

--- prueba.js
+++ prueba.js
@@ -2,4 +2,5 @@ alert ("Este es un mensaje de prueba");
 alert ("Mensaje de prueba #2");
 var  a,b,c,
 suma=a+b
-alert ("Este es el resultado" + suma);
+alert ("Este es el resultado de la suma" + suma);
+alert("Esta es una segunda alerta");
